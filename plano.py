74bcdb173003612c8c6f35728455f4c385d45523
breninhoorb02-spec <breninhoorb02@gmail.com>
2025-12-24 15:09:56 -0300

    Update plano.py

--- plano.py
+++ plano.py
@@ -1,11 +1,12 @@
+
 import streamlit as st
 
-def verificar_limite():
-    if "uploads" not in st.session_state:
-        st.session_state.uploads = 0
+def verificar_plano():
+    if "plano" not in st.session_state:
+        st.session_state.plano = "free"
 
-    st.session_state.uploads += 1
+    return st.session_state.plano
 
-    if st.session_state.uploads > 1:
-        st.warning("Plano gratuito permite apenas 1 PDF.")
-        st.stop()
+def mostrar_upgrade():
+    st.warning("🔒 Recurso disponível apenas para planos pagos")
+    st.markdown("👉 Faça upgrade na landing page")
